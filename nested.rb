128858f5cc5a19a2a8a31e4c0cb1e914502395b3
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-08 18:50:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -112,9 +112,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     alans_known_for = programmer_hash[:alan_kay][:known_for]
+     alans_known_for = changing_alan[:alan_kay][:known_for]
      alans_known_for = alans_new_info
-     programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_to_dennis
