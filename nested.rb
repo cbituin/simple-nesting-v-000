cb181c7b2e3c1defe4d91690035499162fe6966d
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-08 18:49:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -114,7 +114,7 @@ def changing_alan
      alans_new_info = "GUI"
      alans_known_for = programmer_hash[:alan_kay][:known_for]
      alans_known_for = alans_new_info
-     
+     programmer_hash
 end
 
 def adding_to_dennis
